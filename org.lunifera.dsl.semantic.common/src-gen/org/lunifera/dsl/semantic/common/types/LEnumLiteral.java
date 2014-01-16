15e070e819c3e5633ee0da57286ef0722a54057c
fpirchner <florian.pirchner@gmail.com>
2014-01-16 19:44:58 +0100

    Added missing semantic common src-gen

---
+++ org.lunifera.dsl.semantic.common/src-gen/org/lunifera/dsl/semantic/common/types/LEnumLiteral.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2011 - 2014, Lunifera GmbH (Gross Enzersdorf), Loetz KG (Heidelberg)
+ *  All rights reserved. This program and the accompanying materials 
+ *  are made available under the terms of the Eclipse Public License v1.0 
+ *  which accompanies this distribution, and is available at 
+ *  http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ *  Based on ideas from Xtext, Xtend, Xcore
+ *   
+ *  Contributors:  
+ *  		Florian Pirchner - Initial implementation 
+ *  
+ */
+package org.lunifera.dsl.semantic.common.types;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>LEnum Literal</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.lunifera.dsl.semantic.common.types.LEnumLiteral#getName <em>Name</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.lunifera.dsl.semantic.common.types.LunTypesPackage#getLEnumLiteral()
+ * @model
+ * @generated
+ */
+public interface LEnumLiteral extends EObject
+{
+  /**
+   * Returns the value of the '<em><b>Name</b></em>' attribute.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Name</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Name</em>' attribute.
+   * @see #setName(String)
+   * @see org.lunifera.dsl.semantic.common.types.LunTypesPackage#getLEnumLiteral_Name()
+   * @model unique="false"
+   * @generated
+   */
+  String getName();
+
+  /**
+   * Sets the value of the '{@link org.lunifera.dsl.semantic.common.types.LEnumLiteral#getName <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Name</em>' attribute.
+   * @see #getName()
+   * @generated
+   */
+  void setName(String value);
+
+} // LEnumLiteral
