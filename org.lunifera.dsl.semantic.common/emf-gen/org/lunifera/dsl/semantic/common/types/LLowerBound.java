aeb7b1e72685a452aa0cfd503d6cd8ade84a6da6
Klemens Edler <klemens.edler@gmail.com>
2014-12-04 13:58:42 +0100

    Removed evil gitignores

---
+++ org.lunifera.dsl.semantic.common/emf-gen/org/lunifera/dsl/semantic/common/types/LLowerBound.java
@@ -0,0 +1,327 @@
+/**
+ * Copyright (c) 2011 - 2014, Lunifera GmbH (Gross Enzersdorf), Loetz KG (Heidelberg)
+ *  All rights reserved. This program and the accompanying materials 
+ *  are made available under the terms of the Eclipse Public License v1.0 
+ *  which accompanies this distribution, and is available at 
+ *  http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ *  Based on ideas from Xtext, Xtend, Xcore
+ *   
+ *  Contributors:  
+ *  		Florian Pirchner - Initial implementation 
+ *  
+ */
+package org.lunifera.dsl.semantic.common.types;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.emf.common.util.Enumerator;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the literals of the enumeration '<em><b>LLower Bound</b></em>',
+ * and utility methods for working with them.
+ * <!-- end-user-doc -->
+ * @see org.lunifera.dsl.semantic.common.types.LunTypesPackage#getLLowerBound()
+ * @model
+ * @generated
+ */
+public enum LLowerBound implements Enumerator {
+	/**
+	 * The '<em><b>NULL</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #NULL_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	NULL(0, "NULL", "NULL"),
+
+	/**
+	 * The '<em><b>MANY</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #MANY_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	MANY(1, "MANY", "MANY"),
+
+	/**
+	 * The '<em><b>OPTIONAL</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #OPTIONAL_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	OPTIONAL(2, "OPTIONAL", "OPTIONAL"),
+
+	/**
+	 * The '<em><b>ATLEASTONE</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #ATLEASTONE_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	ATLEASTONE(3, "ATLEASTONE", "ATLEASTONE"),
+
+	/**
+	 * The '<em><b>ZERO</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #ZERO_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	ZERO(4, "ZERO", "ZERO"),
+
+	/**
+	 * The '<em><b>ONE</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #ONE_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	ONE(5, "ONE", "ONE");
+
+	/**
+	 * The '<em><b>NULL</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>NULL</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #NULL
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int NULL_VALUE = 0;
+
+	/**
+	 * The '<em><b>MANY</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>MANY</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #MANY
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int MANY_VALUE = 1;
+
+	/**
+	 * The '<em><b>OPTIONAL</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>OPTIONAL</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #OPTIONAL
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int OPTIONAL_VALUE = 2;
+
+	/**
+	 * The '<em><b>ATLEASTONE</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>ATLEASTONE</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #ATLEASTONE
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int ATLEASTONE_VALUE = 3;
+
+	/**
+	 * The '<em><b>ZERO</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>ZERO</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #ZERO
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int ZERO_VALUE = 4;
+
+	/**
+	 * The '<em><b>ONE</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>ONE</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #ONE
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int ONE_VALUE = 5;
+
+	/**
+	 * An array of all the '<em><b>LLower Bound</b></em>' enumerators.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static final LLowerBound[] VALUES_ARRAY =
+		new LLowerBound[] {
+			NULL,
+			MANY,
+			OPTIONAL,
+			ATLEASTONE,
+			ZERO,
+			ONE,
+		};
+
+	/**
+	 * A public read-only list of all the '<em><b>LLower Bound</b></em>' enumerators.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final List<LLowerBound> VALUES = Collections.unmodifiableList(Arrays.asList(VALUES_ARRAY));
+
+	/**
+	 * Returns the '<em><b>LLower Bound</b></em>' literal with the specified literal value.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static LLowerBound get(String literal) {
+		for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+			LLowerBound result = VALUES_ARRAY[i];
+			if (result.toString().equals(literal)) {
+				return result;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the '<em><b>LLower Bound</b></em>' literal with the specified name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static LLowerBound getByName(String name) {
+		for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+			LLowerBound result = VALUES_ARRAY[i];
+			if (result.getName().equals(name)) {
+				return result;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the '<em><b>LLower Bound</b></em>' literal with the specified integer value.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static LLowerBound get(int value) {
+		switch (value) {
+			case NULL_VALUE: return NULL;
+			case MANY_VALUE: return MANY;
+			case OPTIONAL_VALUE: return OPTIONAL;
+			case ATLEASTONE_VALUE: return ATLEASTONE;
+			case ZERO_VALUE: return ZERO;
+			case ONE_VALUE: return ONE;
+		}
+		return null;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private final int value;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private final String name;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private final String literal;
+
+	/**
+	 * Only this class can construct instances.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private LLowerBound(int value, String name, String literal) {
+		this.value = value;
+		this.name = name;
+		this.literal = literal;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public int getValue() {
+	  return value;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getName() {
+	  return name;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getLiteral() {
+	  return literal;
+	}
+
+	/**
+	 * Returns the literal value of the enumerator, which is its string representation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		return literal;
+	}
+	
+} //LLowerBound
