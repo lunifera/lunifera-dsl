0cd9a034e1d818ccd649ebff30cb0c9f2a98220d
Klemens Edler <klemens.edler@gmail.com>
2015-01-05 09:57:13 +0100

    Moved to 0.8.0

--- org.lunifera.dsl.semantic.entity.edit/src/org/lunifera/dsl/semantic/entity/provider/LBeanAttributeItemProvider.java
+++ org.lunifera.dsl.semantic.entity.edit/src/org/lunifera/dsl/semantic/entity/provider/LBeanAttributeItemProvider.java
@@ -453,6 +453,11 @@ public class LBeanAttributeItemProvider extends LBeanFeatureItemProvider {
 		newChildDescriptors.add
 			(createChildParameter
 				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
+				 XAnnotationsFactory.eINSTANCE.createXAnnotation()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
 				 XbaseFactory.eINSTANCE.createXIfExpression()));
 
 		newChildDescriptors.add
@@ -594,11 +599,6 @@ public class LBeanAttributeItemProvider extends LBeanFeatureItemProvider {
 			(createChildParameter
 				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
 				 XbaseFactory.eINSTANCE.createXSynchronizedExpression()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
-				 XAnnotationsFactory.eINSTANCE.createXAnnotation()));
 	}
 
 	private static final class AddImportForDatatypeCommand extends
