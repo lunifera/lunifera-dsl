6c01b0af7d618d6ca6779200b83fa6eccf29a3ed
Florian Pirchner <florian.pirchner@gmail.com>
2015-02-26 13:20:11 +0100

    Fixed samples

--- org.lunifera.dsl.semantic.common/emf-gen/org/lunifera/dsl/semantic/common/types/impl/LunTypesPackageImpl.java
+++ org.lunifera.dsl.semantic.common/emf-gen/org/lunifera/dsl/semantic/common/types/impl/LunTypesPackageImpl.java
@@ -344,9 +344,9 @@ public class LunTypesPackageImpl extends EPackageImpl implements LunTypesPackage
 		isInited = true;
 
 		// Initialize simple dependencies
-		EcorePackage.eINSTANCE.eClass();
-		XAnnotationsPackage.eINSTANCE.eClass();
 		XbasePackage.eINSTANCE.eClass();
+		XAnnotationsPackage.eINSTANCE.eClass();
+		EcorePackage.eINSTANCE.eClass();
 
 		// Create package meta-data objects
 		theLunTypesPackage.createPackageContents();
--- org.lunifera.dsl.semantic.entity.edit/src/org/lunifera/dsl/semantic/entity/provider/LEntityAttributeItemProvider.java
+++ org.lunifera.dsl.semantic.entity.edit/src/org/lunifera/dsl/semantic/entity/provider/LEntityAttributeItemProvider.java
@@ -463,11 +463,6 @@ public class LEntityAttributeItemProvider extends LEntityFeatureItemProvider {
 		newChildDescriptors.add
 			(createChildParameter
 				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
-				 XAnnotationsFactory.eINSTANCE.createXAnnotation()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
 				 XbaseFactory.eINSTANCE.createXIfExpression()));
 
 		newChildDescriptors.add
@@ -609,6 +604,11 @@ public class LEntityAttributeItemProvider extends LEntityFeatureItemProvider {
 			(createChildParameter
 				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
 				 XbaseFactory.eINSTANCE.createXSynchronizedExpression()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
+				 XAnnotationsFactory.eINSTANCE.createXAnnotation()));
 
 		newChildDescriptors.add
 			(createChildParameter
