140f70d9797125fbdff2b53f7bc6144fd6d0c51a
Sabrina Hopf <sabrina.hopf@lunifera.com>
2015-04-04 09:55:22 +0200

    Fixed missing Copyrights for Lunifera GmbH and Loetz KG.

--- org.lunifera.dsl.dto.xtext.ui/src/org/lunifera/dsl/dto/xtext/ui/labeling/DtoGrammarLabelProvider.java
+++ org.lunifera.dsl.dto.xtext.ui/src/org/lunifera/dsl/dto/xtext/ui/labeling/DtoGrammarLabelProvider.java
@@ -1,11 +1,12 @@
 /**
- * Copyright (c) 2011 - 2014, Lunifera GmbH (Gross Enzersdorf), Hans Georg Glöckler, Loetz KG (Heidelberg)
+ * Copyright (c) 2011 - 2015, Lunifera GmbH (Gross Enzersdorf), Loetz KG (Heidelberg)
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Based on org.eclipse.xtend.ide.labeling.XtendLabelProvider
+ *
+ * Contributors:
+ *         Florian Pirchner - Initial implementation
  */
 package org.lunifera.dsl.dto.xtext.ui.labeling;
 
