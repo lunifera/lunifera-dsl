0cafe2b11fa826b6ed45bd22c3b15ee2b16d2af5
Florian Pirchner <florian.pirchner@gmail.com>
2015-06-11 14:49:21 +0200

    Fixed bugs

--- org.lunifera.dsl.semantic.entity.edit/src/org/lunifera/dsl/semantic/entity/provider/LBeanAttributeItemProvider.java
+++ org.lunifera.dsl.semantic.entity.edit/src/org/lunifera/dsl/semantic/entity/provider/LBeanAttributeItemProvider.java
@@ -482,11 +482,6 @@ public class LBeanAttributeItemProvider extends LBeanFeatureItemProvider {
 		newChildDescriptors.add
 			(createChildParameter
 				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
-				 XAnnotationsFactory.eINSTANCE.createXAnnotation()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
 				 XbaseFactory.eINSTANCE.createXIfExpression()));
 
 		newChildDescriptors.add
@@ -628,6 +623,11 @@ public class LBeanAttributeItemProvider extends LBeanFeatureItemProvider {
 			(createChildParameter
 				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
 				 XbaseFactory.eINSTANCE.createXSynchronizedExpression()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
+				 XAnnotationsFactory.eINSTANCE.createXAnnotation()));
 
 		newChildDescriptors.add
 			(createChildParameter
--- org.lunifera.dsl.semantic.entity.edit/src/org/lunifera/dsl/semantic/entity/provider/LEntityAttributeItemProvider.java
+++ org.lunifera.dsl.semantic.entity.edit/src/org/lunifera/dsl/semantic/entity/provider/LEntityAttributeItemProvider.java
@@ -489,11 +489,6 @@ public class LEntityAttributeItemProvider extends LEntityFeatureItemProvider {
 		newChildDescriptors.add
 			(createChildParameter
 				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
-				 XAnnotationsFactory.eINSTANCE.createXAnnotation()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
 				 XbaseFactory.eINSTANCE.createXIfExpression()));
 
 		newChildDescriptors.add
@@ -635,6 +630,11 @@ public class LEntityAttributeItemProvider extends LEntityFeatureItemProvider {
 			(createChildParameter
 				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
 				 XbaseFactory.eINSTANCE.createXSynchronizedExpression()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(LunTypesPackage.Literals.LATTRIBUTE__DERIVED_GETTER_EXPRESSION,
+				 XAnnotationsFactory.eINSTANCE.createXAnnotation()));
 
 		newChildDescriptors.add
 			(createChildParameter
